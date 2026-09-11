5098de6ee8e6a4dd87ffeea7faee25f0c904fc7b
Ankit Samriwal <ankitsamriwal@gmail.com>
2026-09-11 10:07:08 +0400

    Add 59 vocab words in 11 themed packs, Story Corner with 6 Abir & Kashvi stories

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE='wonderbyte-v5';
+const CACHE='wonderbyte-v6';
 const ASSETS=['.','index.html','manifest.webmanifest','icon.svg','icon-180.png','icon-192.png','icon-512.png'];
 self.addEventListener('install',e=>{e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)));self.skipWaiting();});
 self.addEventListener('activate',e=>{e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>clients.claim()));});
